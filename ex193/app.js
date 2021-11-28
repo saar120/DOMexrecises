89b5f275034e4dae3c41be2fa3f1e31c1bddbca7
saar120 <saar120@gmail.com>
2021-11-28 19:48:21 +0200

    ex 193

--- ex193/app.js
+++ ex193/app.js
@@ -1,57 +1,61 @@
 "use strict";
 
-console.log("hello");
-
-const userInputField = document.querySelectorAll("input");
-userInputField.forEach((input, index) => {
-  input.addEventListener("paste", (event) => {
-    let clipboard = event.clipboardData.getData("text").split("");
-    paste(clipboard, index);
-  });
-  input.addEventListener("input", () => {
-    nextValue(index);
+const userCells = document.querySelectorAll("input");
+userCells.forEach((cell, cellIndex) => {
+  cell.addEventListener("input", () => {
+    nextValue(cellIndex);
     validate();
+  });
+
+  cell.addEventListener("paste", (event) => {
+    let clipboard = event.clipboardData.getData("text").split("");
+    paste(clipboard, cellIndex);
+  });
+
+  cell.addEventListener("keydown", (event) => {
+    deleteKey(event, cellIndex);
   });
 });
 
-console.log(userInputField[0].value);
+console.log(userCells[0].value);
 
-function nextValue(index) {
-  let maxLength = userInputField[index].getAttribute("maxlength");
-  let inputLen = userInputField[index].value.length;
-  if (index === userInputField.length - 1) {
+function nextValue(cellIndex) {
+  if (cellIndex === userCells.length - 1) {
     return;
-  } else {
-    let next = userInputField[index].nextElementSibling;
-    if (+inputLen === +maxLength) {
-      next.focus();
-    }
+  }
+  if (userCells[cellIndex].value) {
+    userCells[cellIndex + 1].focus();
   }
 }
 
 function paste(clipboard, currentCell) {
-  const numbersToFill = clipboard.length - currentCell - 1;
-  for (let i = 0; i <= numbersToFill; i++) {
-    userInputField[currentCell].value = clipboard[i];
+  let numbersToFill = userCells.length - currentCell;
+  if (clipboard.length < numbersToFill) {
+    numbersToFill = clipboard.length;
+  }
+  for (let i = 0; i < numbersToFill; i++) {
+    userCells[currentCell].value = clipboard[i];
     currentCell++;
   }
 }
 
 function validate() {
-  userInputField.forEach((input) => {
+  userCells.forEach((input) => {
     input.style.outline = null;
   });
   const password = [1, 2, 3, 4, 5, 6];
-  if (
-    +userInputField[0].value === password[0] &&
-    +userInputField[1].value === password[1] &&
-    +userInputField[2].value === password[2] &&
-    +userInputField[3].value === password[3] &&
-    +userInputField[4].value === password[4] &&
-    +userInputField[5].value === password[5]
-  ) {
-    userInputField.forEach((input) => {
-      input.style.outline = "1px solid green";
-    });
+  for (let i = 0; i < password.length; i++) {
+    if (+userCells[i].value !== password[i]) {
+      return false;
+    }
+  }
+  userCells.forEach((input) => {
+    input.style.outline = "1px solid green";
+  });
+}
+
+function deleteKey(e, cellIndex) {
+  if (cellIndex !== 0 && e.keyCode === 8 && userCells[cellIndex].value == "") {
+    userCells[cellIndex - 1].focus();
   }
 }
