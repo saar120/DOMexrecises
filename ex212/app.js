8af472acb727c217466c87e08f64389f121dd1e4
saar120 <saar120@gmail.com>
2021-11-29 13:03:33 +0200

    ex212

---
+++ ex212/app.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// selectors
+
+let text = document.querySelector("p");
+const plusBtn = document.querySelector(".plus");
+const minusBtn = document.querySelector(".minus");
+
+// event listeners
+
+plusBtn.addEventListener("click", incFontSize);
+minusBtn.addEventListener("click", decFontSize);
+
+// functions
+
+function incFontSize() {
+  let currentFontSize = window.getComputedStyle(text, null).getPropertyValue("font-size");
+  currentFontSize = parseInt(currentFontSize);
+  if (currentFontSize + 10 > 100) {
+    text.style.fontSize = `100px`;
+    return;
+  }
+  text.style.fontSize = `${currentFontSize + 10}px`;
+}
+function decFontSize() {
+  let currentFontSize = window.getComputedStyle(text, null).getPropertyValue("font-size");
+  currentFontSize = parseInt(currentFontSize);
+  if (currentFontSize - 10 < 6) {
+    text.style.fontSize = `6px`;
+    return;
+  }
+  text.style.fontSize = `${currentFontSize - 10}px`;
+}
