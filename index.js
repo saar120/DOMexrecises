20101ef7a13da3b2db6972acda5dc88168eaa24e
saar120 <saar120@gmail.com>
2021-12-05 13:42:52 +0200

    superPowers

--- index.js
+++
@@ -1,7 +0,0 @@
-let startTime = performance.now();
-let counter = 0;
-for (let i = 0; i < 10000000; i++) {
-  counter++;
-}
-let endTime = performance.now();
-console.log(`It took ${endTime - startTime} milliseconds`);
